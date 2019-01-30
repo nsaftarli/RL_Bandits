c48d3934536a3811f6b5d8afffc5f290d44c4098
Nariman Saftarli <nsaftarli@ryerson.ca>
2019-01-30 14:48:50 -0500

    Added code for checking % of optimal moves

--- Q1.py
+++ Q1.py
@@ -10,6 +10,9 @@ def main():
     nTimesChosen = np.zeros((10,))
     trueDistrib = np.random.rand(nArms,)
     estDistrib = np.full((nArms,), 1/nArms)
+    optimalAction = np.argmax(trueDistrib)
+    nTimesOptimal = 0
+    optimalHistory = []
 
     config['c'] = c
     config['nArms'] = nArms
@@ -27,14 +30,23 @@ def main():
             armPulled, nTimesChosen = actionSelect('ucb', config)
             config['nTimesChosen'] = nTimesChosen
 
+        if armPulled == optimalAction:
+            nTimesOptimal += 1
+
+        if t % 100 == 0:
+            optPercent = (nTimesOptimal/t) * 100
+            optimalHistory.append(optPercent)
+            print("Optimal choice was made " + str(optPercent) + "% of the time")
         config['armPulled'] = armPulled
         rewardTable = rollReward(config)
         estDistrib = updateProbs(rewardTable, estDistrib, t)
         config['estDistrib'] = estDistrib
 
     plotHistograms(trueDistrib, estDistrib)
+    plotOptimal(optimalHistory)
 
 def plotHistograms(true, est):
+    plt.suptitle("Real and Predicted Probability Distribution for 10-Armed Bandit Problem Using UCB Action Selection", fontsize=14)
     plt.subplot(1, 2, 1)
     plt.title("True Distribution")
     plt.plot(true)
@@ -42,6 +54,16 @@ def plotHistograms(true, est):
     plt.title("Estimated Distribution")
     plt.plot(est)
     plt.show()
+
+def plotOptimal(hist):
+    plt.suptitle("Frequency of Optimal Actions for 10-Armed Bandit Problem using UCB Action Selection", fontsize=14)
+    plt.plot(hist)
+    plt.show()
+
+
+
+
+
 
 if __name__ == '__main__':
     main()
