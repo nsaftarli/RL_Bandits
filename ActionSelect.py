406721ca77f28b9e87be0d96258f57710db5fdef
Nariman Saftarli <nsaftarli@ryerson.ca>
2019-01-30 11:20:13 -0500

    Moved actions to different file

---
+++ ActionSelect.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+def actionSelect(action, config):
+    if action == 'egreedy':
+        indPicked = greedyActionSelection(config)
+    elif action == 'ucb':
+        pass
+
+    return indPicked
+
+def greedyActionSelection(config):
+    eps = config['eps']
+    isGreedy = rollGreedy(eps)
+
+    indPicked = greedyArm(config) if isGreedy else randomArm(config)
+    return indPicked
+
+
+def rollGreedy(eps):
+    return True if np.random.rand(1)[0] <= eps else False
+
+
+# Action to take if making a greedy move
+def greedyArm(config):
+    # Pick the indices from the estimated distribution with the highest values
+    # If more than one of the same highest value, pick one at random
+    # Take the arm with a probability corresponding to the true distribution
+    trueDistrib = config['trueDistrib']
+    estDistrib = config['estDistrib']
+    t = config['t']
+    maxInds = np.argwhere(estDistrib == np.amax(estDistrib))
+    if len(maxInds) > 1:
+        indPicked = maxInds[np.random.randint(0, len(maxInds))]
+        armPicked = trueDistrib[np.squeeze(indPicked)]
+    else:
+        indPicked = maxInds[0][0]
+        armPicked = trueDistrib[indPicked]
+
+    return indPicked
+
+
+# Action to take if making a random move
+def randomArm(config):
+    # Pick an index at random.
+    estDistrib = config['estDistrib']
+    indPicked = np.random.randint(0, len(estDistrib))
+    return indPicked
+
+
+
+
+
+
